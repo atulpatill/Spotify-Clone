06a9872efe0cee8e64ee67183e2236e9a8910cd8
atul patil <atulpatil539@gmail.com>
2021-12-25 23:08:42 +0530

    feat: updated

---
+++ songs/index.js
@@ -0,0 +1,10 @@
+var songName = document.querySelector("#input")
+var clickBtn = document.querySelector("#btn")
+
+
+function clickHandle(){
+    console.log("click")
+}
+
+
+clickBtn.addEventListener('click',clickHandle)
