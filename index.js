7e0fbc40dbf87f093976a41a1532a419e58aa5a1
atul patil <atulpatil539@gmail.com>
2021-12-26 12:31:03 +0530

    refactor

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const inputTxt = document.querySelector("#input")
 const clickBtn = document.querySelector("#btn")
 const outputDiv = document.querySelector("#output")
 
-function clickHandle(){
-    inputValue = inputTxt.value
-outputDiv.innerText = inputValue 
-    console.log("Clicked")
-}
 
-clickBtn.addEventListener("click", clickHandle)
+clickBtn.addEventListener("click", ()=>{
+    inputValue = inputTxt.value
+    outputDiv.innerText = inputValue 
+        console.log("Clicked")
+})
